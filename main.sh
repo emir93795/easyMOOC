c31879400c190b9639485436136cac6eb736f00d
emir93795 <emir93795@uoc.edu>
2014-10-27 12:21:57 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,20 +6,52 @@ menu(){
     FGRED=`echo "\033[41m"`
     RED_TEXT=`echo "\033[31m"`
     ENTER_LINE=`echo "\033[33m"`
+    
     echo -e "${MENU}*********************************************${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 1)${MENU} Execute and configure ansible directly in my computer ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU} Execute and configure ansible directly in my computer **${NORMAL}"
     echo -e "${MENU}*                                           *${NORMAL}"
-    echo -e "${MENU}**${NUMBER} 2)${MENU} Create an amazon EC2 instance and configure ansible within ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU} Create an amazon EC2 instance and configure ansible within **${NORMAL}"
+    echo -e "${MENU}**${NUMBER}${MENU} (Having an amazon account and a key to acces the instance is strictly necessari!) **${NORMAL}"
     echo -e "${MENU}*                                           *${NORMAL}"
     echo -e "${MENU}*********************************************${NORMAL}"
     echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
     read opt
 }
+
 function option_picked() {
     COLOR='\033[01;31m' # bold red
     RESET='\033[00;00m' # normal white
     MESSAGE=${@:-"${RESET}Error: No message passed"}
     echo -e "${COLOR}${MESSAGE}${RESET}"
+}
+ansibleInstallation(){
+    if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        yum install -y git;
+    fi
+    git clone git@github.com:ansible/ansible.git
+    cd ansible
+    
+    #Setting up the environment variables for ansible
+    if [ -n "$BASH_SOURCE" ] ; then
+    HACKING_DIR=`dirname $BASH_SOURCE`
+    elif [ $(basename $0) = "env-setup" ]; then
+    HACKING_DIR=`dirname $0`
+    else
+    HACKING_DIR="$PWD/hacking"
+    fi
+
+    FULL_PATH=`python -c "import os; print(os.path.realpath('$HACKING_DIR'))"`
+    ANSIBLE_HOME=`dirname "$FULL_PATH"`
+
+    destdir=/etc/environtment
+    sudo sh -c "echo 'PATH=$PATH' >> '$destdir'"
+    sudo sh -c "echo 'PYTHONPATH=$PYTHONPATH' >> '$destdir'"
+    sudo sh -c "echo 'MANPATH=$MANPATH' >> '$destdir'"
+    
+    
+
+    
 }
 clear
 menu
@@ -30,7 +62,8 @@ while [ opt != '' ]
     else
         case $opt in
         1) clear;
-        option_picked "Option 1 Picked";
+        option_picked "Beginning work...";
+        
         menu;
         ;;
 
