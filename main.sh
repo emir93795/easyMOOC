6f9a376d83c83f4440ab28dc9157687a47d60291
emir93795 <emir93795@uoc.edu>
2014-10-27 10:30:11 +0100

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+title=""
+prompt="Pick an option:"
+options=("1. Execute and configure ansible directly in my computer" "2. Create an amazon EC2 instance and configure ansible within")
+
+echo "$title"
+PS2="$prompt "
+select opt in "${options[@]}" "Quit"; do
+  
+    case "$REPLY" in
+    
+    1 ) echo "You picked $opt"
+    2 ) echo "You picked $opt"
+    
+    $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+    *) echo "Invalid option. Try another one.";continue;;
+
+    esac
+
+done
