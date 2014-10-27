f645e594267ae3f203e19004ab39824da5a680b9
emir93795 <emir93795@uoc.edu>
2014-10-27 20:33:45 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -24,34 +24,13 @@ function option_picked() {
     MESSAGE=${@:-"${RESET}Error: No message passed"}
     echo -e "${COLOR}${MESSAGE}${RESET}"
 }
-ansibleInstallation(){
+function ansibleInstallation(){
     if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
     then
         yum install -y git;
     fi
-    git clone git@github.com:ansible/ansible.git
-    cd ansible
-    
-    #Setting up the environment variables for ansible
-    if [ -n "$BASH_SOURCE" ] ; then
-    HACKING_DIR=`dirname $BASH_SOURCE`
-    elif [ $(basename $0) = "env-setup" ]; then
-    HACKING_DIR=`dirname $0`
-    else
-    HACKING_DIR="$PWD/hacking"
-    fi
-
-    FULL_PATH=`python -c "import os; print(os.path.realpath('$HACKING_DIR'))"`
-    ANSIBLE_HOME=`dirname "$FULL_PATH"`
-
-    destdir=/etc/environtment
-    sudo sh -c "echo 'PATH=$PATH' >> '$destdir'"
-    sudo sh -c "echo 'PYTHONPATH=$PYTHONPATH' >> '$destdir'"
-    sudo sh -c "echo 'MANPATH=$MANPATH' >> '$destdir'"
-    
-    
-
-    
+    rpm -ivh --force http://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+    yum install -y ansible
 }
 clear
 menu
@@ -63,6 +42,7 @@ while [ opt != '' ]
         case $opt in
         1) clear;
         option_picked "Beginning work...";
+        ansibleInstallation();
         
         menu;
         ;;
