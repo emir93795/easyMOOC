241cc604cb4fff6986310f991274137152d908cf
emir93795 <emir93795@uoc.edu>
2014-10-27 10:31:40 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ prompt="Pick an option:"
 options=("1. Execute and configure ansible directly in my computer" "2. Create an amazon EC2 instance and configure ansible within")
 
 echo "$title"
-PS2="$prompt "
+PS3="$prompt "
 select opt in "${options[@]}" "Quit"; do
   
     case "$REPLY" in
