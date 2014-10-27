e537cf11a108fba3508703ad03839314394507d8
emir93795 <emir93795@uoc.edu>
2014-10-27 10:41:34 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
-title=""
-prompt="Pick an option:"
-options=("1. Execute and configure ansible directly in my computer" "2. Create an amazon EC2 instance and configure ansible within")
+menu(){
+    NORMAL=`echo "\033[m"`
+    MENU=`echo "\033[36m"` #Blue
+    NUMBER=`echo "\033[33m"` #yellow
+    FGRED=`echo "\033[41m"`
+    RED_TEXT=`echo "\033[31m"`
+    ENTER_LINE=`echo "\033[33m"`
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU} Execute and configure ansible directly in my computer ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU} Create an amazon EC2 instance and configure ansible within ${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 5)${MENU} ${NORMAL}"
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
+    read opt
+}
+function option_picked() {
+    COLOR='\033[01;31m' # bold red
+    RESET='\033[00;00m' # normal white
+    MESSAGE=${@:-"${RESET}Error: No message passed"}
+    echo -e "${COLOR}${MESSAGE}${RESET}"
+}
 
-echo "$title"
-PS3="$prompt "
-select opt in "${options[@]}" "Quit"; do
-  
-    case "$REPLY" in
-    
-    1 ) echo "You picked $opt";;
-    2 ) echo "You picked $opt";;
-    
-    $(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
-    *) echo "Invalid option. Try another one.";continue;;
+clear
+show_menu
+while [ opt != '' ]
+    do
+    if [[ $opt = "" ]]; then 
+            exit;
+    else
+        case $opt in
+        1) clear;
+        option_picked "Option 1 Picked";
+        menu;
+        ;;
 
+        2) clear;
+            option_picked "Option 2 Picked";
+        menu;
+            ;;
+
+        x)exit;
+        ;;
+
+        \n)exit;
+        ;;
+
+        *)clear;
+        option_picked "Pick an option from the menu";
+        show_menu;
+        ;;
     esac
-
+fi
 done
