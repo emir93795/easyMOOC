48deb9b8f2663d661e7c1925b0792a081e9305f6
emir93795 <emir93795@uoc.edu>
2014-10-28 09:07:56 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-menu(){
+
+#Available colors (Seen in stackoverflow.com)
     NORMAL=`echo "\033[m"`
     MENU=`echo "\033[36m"` #Blue
     NUMBER=`echo "\033[33m"` #yellow
@@ -7,11 +8,26 @@ menu(){
     RED_TEXT=`echo "\033[31m"`
     ENTER_LINE=`echo "\033[33m"`
     
+menu(){
     echo -e "${MENU}*********************************************${NORMAL}"
     echo -e "${MENU}**${NUMBER} 1)${MENU} Execute and configure ansible directly in my computer **${NORMAL}"
     echo -e "${MENU}*                                           *${NORMAL}"
     echo -e "${MENU}**${NUMBER} 2)${MENU} Create an amazon EC2 instance and configure ansible within **${NORMAL}"
     echo -e "${MENU}**${NUMBER}${MENU} (Having an amazon account and a key to acces the instance is strictly necessari!) **${NORMAL}"
+    echo -e "${MENU}*                                           *${NORMAL}"
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
+    read opt
+}
+menu2(){
+    echo -e "${MENU}*********************************************${NORMAL}"
+    echo -e "${MENU}**What kind of MOOC management system are you going to use?**${NORMAL}"
+    echo -e "${MENU}*                                           *${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 1)${MENU} Install Moodle **${NORMAL}"
+    echo -e "${MENU}*                                           *${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 2)${MENU} Install Canvas **${NORMAL}"
+    echo -e "${MENU}*                                           *${NORMAL}"
+    echo -e "${MENU}**${NUMBER} 3)${MENU} Install edX **${NORMAL}"   
     echo -e "${MENU}*                                           *${NORMAL}"
     echo -e "${MENU}*********************************************${NORMAL}"
     echo -e "${ENTER_LINE}Please enter a menu option and enter or ${RED_TEXT}enter to exit. ${NORMAL}"
@@ -24,14 +40,21 @@ function option_picked() {
     MESSAGE=${@:-"${RESET}Error: No message passed"}
     echo -e "${COLOR}${MESSAGE}${RESET}"
 }
+
+#Function that installs ansible
 function ansibleInstallation(){
-    if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
-    then
-        sudo yum install -y git;
-    fi
     sudo rpm -ivh --force http://www.mirrorservice.org/sites/dl.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
     sudo yum install -y ansible
+    #Check if ansible is correctly installed
+    if yum list installed ansible >/dev/null 2>&1; then
+        echo -e "${NUMBER}Ansible was correctly installed.${NORMAL}"
+    else   
+        echo -e "${RED_TEXT}Ansible was not correctly installed.${NORMAL}"
+    fi
 }
+
+ #Funtion that installs moodle using ansible
+#Beginning menu
 clear
 menu
 while [ opt != '' ]
@@ -43,13 +66,37 @@ while [ opt != '' ]
         1) clear;
         option_picked "Beginning work...";
         ansibleInstallation
-        
-        menu;
+        menu2
+        while [ opt != '' ]
+            do
+            if [[ $opt = "" ]]; then 
+                exit;
+            else
+                case $opt in
+                1) clear;
+                option_picked "Let's go!";
+                ;;
+                2) clear;
+                option_picked "Option 2 Picked";
+                ;;
+                3) clear;
+                option_picked "Option 2 Picked";
+                ;;
+                x)exit;
+                ;;
+                \n)exit;
+                ;;
+                *)clear;
+                option_picked "Pick an option from the menu";
+                menu2;
+                ;;
+                esac
+            fi 
+        done
         ;;
 
         2) clear;
             option_picked "Option 2 Picked";
-        menu;
             ;;
 
         x)exit;
